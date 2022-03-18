583f4116cc6e9faa73b02db4abe6aa9eae2d9a43
구홍지 <kongji9847@gmail.com>
2022-03-18 13:32:53 +0900

    study my code

--- Algorithm_practice/0315/1952_SWEA_수영장/s1.py
+++ Algorithm_practice/0315/1952_SWEA_수영장/s1.py
@@ -1,6 +1,7 @@
-# dp 사용하기
+# dp 사용하기 -> 1칸 혹은 2칸씩 오를 수 있는 계단의 모든 방법의 수와 비슷한 느낌
 import sys
 sys.stdin = open('input.txt')
+
 T = int(input())
 for tc in range(1, T+1):
     prices = list(map(int, input().split()))
@@ -12,15 +13,21 @@ for tc in range(1, T+1):
     for i in range(1, 13):
         costs[i] = min(prices[0] * plan[i], prices[1])
 
+    # 각 인덱스에 해당하는 달까지의 최소 비용 구하기 -> if 4라면 1~4월까지 누적 최소 비용
+    # 이때, 3개월치 이용권 고려 -> 계단을 3칸 올라가는 경우
+    # 한달 이용권 단위로 사용했을 때, or 3개월 이용권 단위 중 최소의 결과를 선택해 해당 지점에서의 최솟값 구하기
+    # 인덱스가 12일 때는, 12월까지의 누적 최소 비용이 구해진다.
     costs[2] += costs[1]
     for j in range(3, 13):
         costs[j] = min(costs[j-1] + costs[j], costs[j-3] + prices[2])
 
+    # 1년치 이용권과 12월 비교
     if costs[-1] > prices[-1]:
         ans = prices[-1]
     else:
         ans = costs[-1]
-    print(costs)
+
+    #print(costs)
     print(f'#{tc}', ans)
 
 
--- Algorithm_practice/0315/1952_SWEA_수영장/s3.py
+++ Algorithm_practice/0315/1952_SWEA_수영장/s3.py
@@ -6,43 +6,54 @@ T = int(input())
 for tc in range(1, T+1):
     prices = list(map(int, input().split()))
     plan = list(map(int, input().split()))
-    plan = [0] + plan + [0, 0]
+    plan = [0] + plan
 
+    # 3개월 이용권을 쓰며 시작하는 것이 가장 이득인 달 목록 구하기
     hint = []
     i = 1
-    while i <= 10:
+    while i <= 10:                                                  # 최소 비용일 때, 3개월 이용권이 11이나 12월에 쓰이는 경우는 굳이 없다.
         cnt = 0
+        # 각 달에서 한달 또는 일일 이용권을 혼합한 조합으로 사용했을 때의 최소 비용 구하기
         for n in range(3):
             if plan[i+n]:
                 if plan[i+n]*prices[0] > prices[1]:
                     cnt += prices[1]
                 else:
                     cnt += prices[0]*plan[i+n]
+
         # 3개월이 가장 이득인 때
         if cnt > prices[2]:
-            hint.append([i, len(hint)-1+1])                 # 시작점, 인덱스
+            hint.append([i, len(hint)-1+1])                 # 시작점, 인덱스 -> 뒤에서 다시 앞으로 돌아오는 것을 막기 위해
         i += 1
-    # print(hint)
-    # 3개월이 가장 이득인데, 이득인게 많으면 좋은거 아냐.....?????? 왜????????? 뭐가 문제지??????
 
-    comb = []
+    # 3개월 이용권을 사용하는 것이 가장 이득인 달의 목록에서 최대한 달이 많은 조합을 뽑자(단, 달이 겹치지 않게) -> dfs 사용
+    comb =[]
     max_num = -1
     for i in range(len(hint)):
         stack = [[hint[i]]]
         while stack:
             k = stack.pop()
-            if 12 in (k[-1][0], k[-1][0] + 1, k[-1][0] + 2) or k[-1][0] + 3 > hint[-1][0] + 2:
-                comb.append(k)
+            if 12 in (k[-1][0], k[-1][0] + 1, k[-1][0] + 2) or k[-1][0] + 3 > hint[-1][0] + 2:              # 12월이 포함된 3개월을 가진 달이거나, 더이상 진행할 곳이 없을 때 현재 조합을 저장
+                if max_num < len(k):
+                    max_num = len(k)
+                    comb = []
+                    comb.append(k)
+                elif max_num == len(k):
+                    comb.append(k)
                 continue
 
-            flag = 0
-            for j in range(k[-1][-1] + 1, len(hint)):
-                if (hint[j][0] >= k[-1][0] + 3):
+            for j in range(k[-1][-1] + 1, len(hint)):                  # 앞으로 다시 돌아와 보는 것을 방지
+                if (hint[j][0] >= k[-1][0] + 3):                        # 겹치는 달이 없게
                     stack.append(k + [hint[j]])
-                    flag = 1
-            comb.append(k)
-    #print(comb)
 
+            if max_num < len(k):
+                max_num = len(k)
+                comb = []
+                comb.append(k)                                          # 종료조건에 해당하지는 않지만 짧게 끝나는 조합
+            elif max_num == len(k):
+                comb.append(k)
+
+    # 한달 단위로 최소 비용 구하기
     min_price = 0
     for month in plan:
         if month:
@@ -51,6 +62,7 @@ for tc in range(1, T+1):
             else:
                 min_price += prices[0] * month
 
+    # 가능한 3개월 조합에서 가장 최소 비용 찾기
     for elements in comb:
         new = deepcopy(plan)
         cnt = 0
